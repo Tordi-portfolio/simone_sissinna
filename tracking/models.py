3782408d270a5740dbc5c89885c577acaee65036
Tordi-portfolio <techwithtordi@gmail.com>
2025-07-12 11:50:50 +0100

    e

--- tracking/models.py
+++ tracking/models.py
@@ -47,6 +47,7 @@ class Shipment(models.Model):
     service = models.CharField(max_length=100, choices=SERVICE_CHOICES)
     commodity = models.CharField(max_length=100, choices=COMMODITY_CHOICES)
     destination_country = models.CharField(max_length=100)
+    current_location = models.CharField(max_length=100, default='Processing', editable=False)
     note = models.TextField(blank=True, null=True)
     status = models.CharField(max_length=20, choices=[
         ('Pending', 'Pending'),
